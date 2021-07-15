76af1171f81555cfa69b13af4e3356f8b7960f58
swapnilg671 <swapnilg671@gmail.com>
2021-07-15 21:14:23 +0530

    with route

--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import App from './component/App';
 
 ReactDOM.render(
   <React.StrictMode>
+    <BrowserRouter>
     <App />
-  </React.StrictMode>,
+    </BrowserRouter>,
+    </React.StrictMode>,
+  
   document.getElementById('root')
 );
 
